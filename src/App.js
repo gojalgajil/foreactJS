0a7398416ec8bcfff49dfacba9c532f9c962d8fe
gojalgajil <kg.gozal@gmail.com>
2025-06-12 14:01:35 +0700

    Foreact JS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>FE React JS - Product List</h1>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
